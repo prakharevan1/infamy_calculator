4f97ca1cd2700d8d3c1c65d84b96fe0d67d3d83a
Evan Dagur <dagurevan@gmail.com>
2025-01-21 12:20:04 -0800

    Basic draft finished, allows for user input

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,72 @@
+use std::io;
 fn main() {
     //parse input
+    let mut start_inf = String::new();
+    let mut end_inf = String::new();
+    let mut cheaper_inf = String::new();
 
+    //parse starting infamy input
 
+    println!("Please enter your starting infamy:");
+    io::stdin()
+        .read_line(&mut start_inf)
+        .expect("Failed to read line. Please restart or unwanted results may occur!");
+    let start_inf: i32 = start_inf
+        .trim()
+        .parse()
+        .expect("Failed to parse input. Please restart or unwanted results may occur!");
 
-    let start_inf = 16;
-    let end_inf = 23;
+    //parse ending infamy input
+
+    println!("Please enter your ending infamy:");
+    io::stdin()
+        .read_line(&mut end_inf)
+        .expect("Failed to read line. Please restart or unwanted results may occur!");
+    let end_inf: i32 = end_inf
+        .trim()
+        .parse()
+        .expect("Failed to parse input. Please restart or unwanted results may occur!");
+
+    //check if cheaper infamy or not
+
+    println!("Do you have cheaper infamy? (Y/N):");
+    io::stdin()
+        .read_line(&mut cheaper_inf)
+        .expect("Failed to get input. Please restart or unwanted results may occur!");
+    let cheaper_inf = cheaper_inf.trim();
+    let cheaper_inf = match cheaper_inf {
+        "Y" => true,
+        "N" => false,
+        "y" => true,
+        "n" => false,
+        _ => false,
+    };
+    //check infamy values
+    let mut cost: i128;
     if start_inf == end_inf || end_inf < start_inf {
         return;
     }
     if end_inf > 25 {
-        above_inf_25(start_inf, end_inf);
+        cost = above_inf_25(start_inf, end_inf);
     } else {
-        below_inf_25(start_inf, end_inf);
+        cost = below_inf_25(start_inf, end_inf);
     }
+    if cheaper_inf == true {
+        cost /= 2;
+    }
+    println!("The total cost to get from infamy {start_inf} to {end_inf} is {cost} dollars");
 }
-fn above_inf_25(start_inf: i32, end_inf: i32) {
-    let start_inf = start_inf; //cloned
-    let end_inf = end_inf; //cloned
-    let mut total = 0;
+fn above_inf_25(start_inf: i32, end_inf: i32) -> i128 {
+    let start_inf: i128 = start_inf as i128; //cloned
+    let end_inf: i128 = end_inf as i128; //cloned, parses it as an i128
+    let mut total: i128 = 0;
 
     //block
     let mut start_price = 20_000_000;
     {
         if start_inf > 25 {
-            for _i in 25..start_inf { //gen start price
+            for _i in 25..start_inf {
+                //gen start price
                 start_price += 10_000_000; //price is 20m
             }
         }
@@ -31,20 +74,20 @@ fn above_inf_25(start_inf: i32, end_inf: i32) {
     //block 2
     {
         total += 10_000_000; //starts off at 30m :)
-        for _i in 25..end_inf - 1{
+        for _i in 25..end_inf - 1 {
             total += start_price;
             start_price += 10_000_000;
         }
-        let mut diff = end_inf - start_inf;
+        let mut diff: i128 = end_inf - start_inf;
         diff *= 20_000_000;
         total += diff;
     }
-    print!("{total}");
+    total
 }
-fn below_inf_25(start_inf: i32, end_inf: i32) {
-    let start_inf = start_inf;
-    let end_inf = end_inf;
-    let mut total = end_inf - start_inf;
+fn below_inf_25(start_inf: i32, end_inf: i32) -> i128 {
+    let start_inf = start_inf as i128; //clones and parses as an i128
+    let end_inf = end_inf as i128; //clones and parses as an 128
+    let mut total: i128 = end_inf - start_inf;
     total *= 20_000_000;
-    println!("{total}");
+    total
 }
